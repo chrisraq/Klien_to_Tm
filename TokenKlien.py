5b08e2449e7995d0d16e5ea22fb92c2c1c606f15
chrisraq <54965761+chrisraq@users.noreply.github.com>
2019-09-17 18:24:11 -0500

    TokenKlien.py

---
+++ TokenKlien.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+class TokenType(Enum):
+    operator    = 0
+    punct       = 1
+    number      = 2
+    keyword     = 3
+    identifier  = 4
+    boolean     = 5
+    eof         = 6
+
+class Token:
+
+    def __init__(self, tokenType, tokenValue = Null):
+        self.tokenType = tokenType
+        self.tokenValue = tokenValue
+    
+    def isOperator(self):
+        return self.tokenType == TokenType.operator
+
+    def isPunct(self):
+        return self.tokenType == TokenType.punct
+
+    def isNumber(self):
+        return self.tokenType == TokenType.number
+
+    def isKeyword(self):
+        return self.tokenType == TokenType.keyword
+
+    def isIdentifier(self):
+        return self.tokenType == TokenType.identifier
+
+    def isBoolean(self):
+        return self.tokenType == TokenType.boolean
+
+    def isEof(self):
+        return self.tokenType == TokenType.eof
+    
+    def value(self):
+        return self.tokenValue
