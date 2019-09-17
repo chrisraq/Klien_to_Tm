a8533711d7606783f8a75cff3da4bd2167206651
chrisraq <54965761+chrisraq@users.noreply.github.com>
2019-09-17 18:19:40 -0500

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -1,15 +1,60 @@
+from enum import Enum
+from tokenKlien import Token, TokenType
+
+class State(Enum):
+    start       = 0
+    operator    = 1
+    punct       = 2
+    zero        = 3
+    number      = 4
+    identifier  = 5
+    comment     = 6
+
 class Scanner:
     "Read tokens from input program"
+    keywords = {"integer" : "Keyword", "boolean" : "Keyword", "if" : "Keyword", "then" : "Keyword", "else" : "Keyword", "not" : "Keyword", "and" : "Keyword", "or" : "Keyword", "function" : "Keyword", "main" : "Identifier", "print" : "Identifier", "true" : "Boolean", "false" : "Boolean"}
 
-    def __init__(self, program_str):
-        self.program_str = program_str
+    def __init__(self, programStr):
+        self.programStr = programStr
         self.pos = 0
-        self.tokens = []
+        self.tokens = self.scan(self.programStr)
 
 
-    def scan(self.program_str):
+    def scan(programStr):
+        alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         accum = ""
-
-        pos = 0
-        while pos < len(self.program_str):
-            
+        state = State.start
+        tokens = []
+        strPos = 0
+        while strPos < len(programStr):
+            if state == State.start:
+                if programStr[strPos].isspace():
+                    pass
+                elif programStr[strPos] in "+-*/<=":
+                    tokens.append(Token(TokenType.operator),programStr[strPos])
+                elif programStr[strPos] in "),:":
+                    tokens.append(Token(TokenType.punct),programStr[strPos])
+                elif programStr[strPos] in "0":
+                    accum = programStr[strPos]
+                    state = State.zero
+                elif programStr[strPos] in "123456789":
+                    accum = programStr[strPos]
+                    state = State.number
+                elif programStr[strPos] in alphabet:
+                    accum = programStr[strPos]
+                    state = State.identifier
+                elif programStr[strPos] == "(":
+                    if (strPos + 1) < len(programStr) -1 and programStr[strPos+1] == "*":
+                        state = State.comment
+                    else:
+                        tokens.append(Token(TokenType.punct),programStr[strPos])
+                else: #Character not exceptable in the language
+                    #Error
+                    pass
+    def next(self):
+        nextToken = self.tokens[self.pos]
+        self.pos += 1
+        return nextToken
+    
+    def peek(self):
+        return self.tokens[self.pos]
